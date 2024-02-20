2c092acea7065a6729855a30d51b7cc6dd6cf0f1
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-20 11:22:24 +0200

    'id' has been removed from 'input' tag.

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -26,27 +26,27 @@ function changeHandler(event) {
         <div>
           <div className="mb-3">
             <label htmlFor="text" className="form-label">First Name</label>
-            <input type="text" name="firstName" className="form-control" id="text" value={form.firstName} onChange={changeHandler} />
+            <input type="text" name="firstName" className="form-control" value={form.firstName} onChange={changeHandler} />
           </div>
           <div className="mb-3">
             <label htmlFor="text" className="form-label">Last Name</label>
-            <input type="text" name="lastName" className="form-control" id="text" value={form.lastName} onChange={changeHandler} />
+            <input type="text" name="lastName" className="form-control" value={form.lastName} onChange={changeHandler} />
           </div>
           <div className="mb-3">
             <label htmlFor="text" className="form-label">Username</label>
-            <input type="text" name="username" className="form-control" id="text" value={form.username} onChange={changeHandler} />
+            <input type="text" name="username" className="form-control" value={form.username} onChange={changeHandler} />
           </div>
           <div className="mb-3">
             <label htmlFor="text" className="form-label">Date Of Birth</label>
-            <input type="date" name="dateOfBirth" className="form-control" id="text" value={form.dateOfBirth} onChange={changeHandler} />
+            <input type="date" name="dateOfBirth" className="form-control" value={form.dateOfBirth} onChange={changeHandler} />
           </div>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Email</label>
-            <input type="email" name="email" className="form-control" id="email" value={form.email} onChange={changeHandler} />
+            <input type="email" name="email" className="form-control" value={form.email} onChange={changeHandler} />
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" name="password" className="form-control" id="password" value={form.password} onChange={changeHandler} />
+            <input type="password" name="password" className="form-control" value={form.password} onChange={changeHandler} />
             <div id="passwordHelpBlock" className="form-text">
               Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
             </div>
