0af900ac5998cdf0520610880303e0d1e72fff51
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-21 17:45:37 +0200

    'AuthContext' has been added and context values have been added as well.

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import useRoutes from './routes/routes';
+import useAuth from './hooks/auth.hook';
+import AuthContext from './context/AuthContext';
 
 function App() {
-  const isAuthenticated = false;
+  const { login, logout, token, id, ready } = useAuth();
+  const isAuthenticated = !!token;
   const routes = useRoutes(isAuthenticated);
 
+  if (!ready) {
+    return (
+      <div></div>
+    );
+  }
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className='container'>
-        {routes}
-      </div>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ login, logout, token, id, isAuthenticated }}>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          {routes}
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
