04a5646fb1ebdc5e6d63ed29d4b91e2f034d4884
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-21 17:09:20 +0200

    useAuth hook has been created to manage user login and logout.

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const storageName = "userData";
+
+function useAuth() {
+  const [token, setToken] = useState(null);
+  const [id, setId] = useState(null);
+  const [ready, setReady] = useState(false);
+
+  function login(jwtToken, userId) {
+    setToken(jwtToken);
+    setId(userId);
+    localStorage.setItem(storageName, JSON.stringify({ token: jwtToken, id: userId }));
+  }
+
+  function logout() {
+    setToken(null);
+    setId(null);
+    localStorage.removeItem(storageName);
+  }
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem(storageName));
+
+    if (data && data.token) {
+      login(data.token, data.id);
+    }
+
+    setReady(true);
+  }, [login]);
+
+  return { login, logout, token, id, ready }
+}
+
+export default useAuth;
