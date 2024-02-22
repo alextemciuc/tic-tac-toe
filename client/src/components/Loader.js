53a35ff43f3382c72b751ede231596fa74f3a354
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-22 12:42:33 +0200

    Width and height have been modified for Loader component.

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <div className="d-flex justify-content-center">
-      <div className="spinner-border" role="status">
+      <div className="spinner-border" style={{ width: '4rem', height: '4rem' }} role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
