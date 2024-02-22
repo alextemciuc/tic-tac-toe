e7a52721ecfa414709facf48939df1fed40ca811
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-22 19:16:02 +0200

    Basic functionality has been created.

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function MainPage() {
+  const { token } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function playHandler() {
+    if (token) {
+      return navigate('/tictactoe');
+    }
+    navigate('/login');
+  }
+
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="row justify-content-center mt-3 bg-primary-subtle">
+      <div className="col-4 pt-3 pb-3">
+        <div className="d-flex justify-content-center">
+          <span>Tic Tac Toe</span>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-primary" onClick={playHandler}>Play</button>
+        </div>
+      </div>
     </div>
   );
 }
