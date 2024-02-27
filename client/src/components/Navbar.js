0c0a6ddcb09ca5cc683f7686890956786ee59219
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-27 12:47:47 +0200

    Navbar was modified.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import UserDropdown from "./UserDropdown";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const { token } = useContext(AuthContext);
+
   return (
     <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
       <div className="container">
-        <a className="navbar-brand" href="/">Tic Tac Toe</a>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <NavLink className="navbar-brand" to="/">Tic Tac Toe</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
           <ul className="navbar-nav column-gap-3">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/tictactoe">Tic Tac Toe</a>
+              <NavLink className="nav-link" to="/tictactoe">Tic Tac Toe</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/login">Sign In</a>
+              { token ? <UserDropdown /> : <NavLink className="nav-link" to="/login">Sign In</NavLink> }
             </li>
           </ul>
         </div>
