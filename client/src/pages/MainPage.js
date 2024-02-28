34d8c504d392ef28e01c67caaba1672399b0ced6
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-02-28 18:56:14 +0200

    Image as a logo has been added.

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import tictactoeLogo from "../images/tic_tac_toe_logo.png";
 
 function MainPage() {
   const { token } = useContext(AuthContext);
@@ -17,7 +18,7 @@ function MainPage() {
     <div className="row justify-content-center mt-3 bg-primary-subtle">
       <div className="col-4 pt-3 pb-3">
         <div className="d-flex justify-content-center">
-          <span>Tic Tac Toe</span>
+          <img src={tictactoeLogo} alt="Tic tac toe logo" style={{ height: "100px", width: "100px", userSelect: "none" }} />
         </div>
         <div className="d-flex justify-content-center">
           <button className="btn btn-primary" onClick={playHandler}>Play</button>
