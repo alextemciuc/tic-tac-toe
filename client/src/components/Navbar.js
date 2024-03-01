5d8d34d326e43106370517774a5164c6ddc25056
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-03-01 17:51:33 +0200

    'tictactoeHandler' function has been created.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import UserDropdown from "./UserDropdown";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import useHttp from "../hooks/http.hook";
 
 function Navbar() {
-  const { token } = useContext(AuthContext);
+  const { token, logout } = useContext(AuthContext);
+  const { request } = useHttp();
+  const navigate = useNavigate();
+
+  async function tictactoeHandler(event) {
+    event.preventDefault();
+    try {
+      await request('/api/auth/check', 'POST', null, {Authorization: `Bearer ${token}`});
+      navigate('/tic-tac-toe');
+    } catch (e) {
+      logout();
+      navigate('/');
+    }
+  }
 
   return (
     <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
@@ -19,7 +33,7 @@ function Navbar() {
               <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/tictactoe">Tic Tac Toe</NavLink>
+              <NavLink className="nav-link" to="/tic-tac-toe" onClick={tictactoeHandler}>Tic Tac Toe</NavLink>
             </li>
             <li className="nav-item">
               { token ? <UserDropdown /> : <NavLink className="nav-link" to="/login">Sign In</NavLink> }
