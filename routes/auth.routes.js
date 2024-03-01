c1106a304d3af21e416d59bf847a3073d4d8694e
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-03-01 17:47:58 +0200

    New request has been created to check token validity.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -86,4 +86,19 @@ router.post(
   }
 );
 
+router.post('/check', (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'User is not logged' });
+    }
+
+    jwt.verify(token, config.get('jwtSecret'));
+    res.json({ message: 'User is logged'});
+  } catch (e) {
+    res.status(401).json({ message: 'User is not logged' });
+  }
+})
+
 module.exports = router;
