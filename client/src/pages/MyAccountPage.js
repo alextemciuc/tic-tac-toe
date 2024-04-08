6884e1a94d0c9bc68a44ddf8f07ffc7c0845bf45
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-04-08 15:17:15 +0300

    Data table has been created.

--- client/src/pages/MyAccountPage.js
+++ client/src/pages/MyAccountPage.js
@@ -2,7 +2,20 @@ import React from "react";
 
 function MyAccountPage() {
   return (
-    <h1>My Account</h1>
+    <div>
+      <table className="table w-50">
+        <tbody>
+          <tr>
+            <th className="border-end" scope="row">Games</th>
+            <td>100</td>
+          </tr>
+          <tr>
+            <th className="border-end" scope="row">Wins</th>
+            <td>50</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
