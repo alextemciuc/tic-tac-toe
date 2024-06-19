de9c83ea6758d70adc303323260003ab931c030d
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-06-19 16:15:31 +0300

    Router to get statistic data has been added.

--- routes/statistic-routes.js
+++ routes/statistic-routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const authMiddleware = require('../middlewares/auth-middleware');
 const statisticController = require('../controllers/statistic-controller');
 const router = Router();
 
 router.post('/generate', statisticController.generate);
+router.get('/', authMiddleware, statisticController.getData);
 
 module.exports = router;
