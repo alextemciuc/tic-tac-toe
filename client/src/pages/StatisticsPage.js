ba65473de886b4d3b27117c80ec079bda30e5c0a
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-06-19 16:14:13 +0300

    Functionality to get statistic data from database has been created.

--- client/src/pages/StatisticsPage.js
+++ client/src/pages/StatisticsPage.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import useHttp from "../hooks/http.hook";
+import AuthContext from "../context/AuthContext";
 
 function StatisticsPage() {
+  const [statistics, setStatistics] = useState({});
+  const { request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const getStatisticsData = useCallback(async () => {
+    const response = await request('/api/statistic', 'GET', null, {Authorization: `Bearer ${token}`});
+    const data = await response.json();
+    setStatistics(data);
+  }, [token, request]);
+
+  useEffect(() => {
+    getStatisticsData()
+  }, [getStatisticsData]);
+
   return (
     <div>
       <table className="table w-50">
         <tbody>
           <tr>
             <th className="border-end" scope="row">Games</th>
-            <td>100</td>
+            <td>{statistics.games}</td>
           </tr>
           <tr>
             <th className="border-end" scope="row">Wins</th>
-            <td>50</td>
+            <td>{statistics.wins}</td>
           </tr>
         </tbody>
       </table>
